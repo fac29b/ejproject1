5b45d727127b2f505a5926e40a1e01394d37445a
josueJURE <josue.jure@gmail.com>
2023-11-05 09:47:57 +0000

    feat(JS): put event listener on homePage element using an if statement

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const footer = document.querySelector("footer");
+const homePage = document.querySelector(".home-page");
+const teamPage = document.querySelector(".team-page");
+const conctactUs = document.querySelector(".contact-us");
+
+
+console.log(homePage, teamPage, conctactUs)
 const currentYear = new Date().getFullYear();
 
 footer.innerHTML = ` Superheros ${currentYear}` 
+
+
+if(homePage) {
+    homePage.addEventListener("click", goToHomePage);
+}
+
+
+// if (searchByGenre) {
+//     searchByGenre.addEventListener("click", goToSearchByGenrePage);
+//   }
+
+//   function goToSearchByGenrePage() {
+//     window.location.assign("/searchByGenrePage.html");
+//   }
