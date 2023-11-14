c749bd76bf0aa1f7ae18a4cf3b8e9a1a89639151
josueJURE <josue.jure@gmail.com>
2023-11-14 10:32:38 +0000

    feat(JS): turn anonymous functions into arrow functions

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // index.html page
 const dots = document.querySelectorAll(".dot");
-
-console.log(dots);
-
 const mySlides = document.querySelectorAll(".my-slides");
 const homePage = document.querySelector(".home-page");
 const slideshowContainer = document.querySelector(".slideshow-container");
@@ -27,7 +24,7 @@ const currentYear = new Date().getFullYear();
 year.innerText = ` Superheros ${currentYear}`;
 
 if (submitBtn) {
-  submitBtn.addEventListener("click", function (e) {
+  submitBtn.addEventListener("click",  (e) => {
     if (form.checkValidity()) {
       requestMessage.style.visibility = "visible";
       contactBody.style.visibility = "hidden";
@@ -86,21 +83,21 @@ function carousel() {
 
   advance(currentSlide);
 
-  next.addEventListener("click", function () {
+  next.addEventListener("click",  () => {
     clearTimeout(clear);
     slideToNext(currentSlide);
   });
 
-  slideshowContainer.addEventListener("mouseenter", function () {
+  slideshowContainer.addEventListener("mouseenter",  () => {
     clearTimeout(clear);
   });
 
-  slideshowContainer.addEventListener("mouseleave", function () {
+  slideshowContainer.addEventListener("mouseleave",  () => {
     clearTimeout(clear);
     advance(currentSlide);
   });
 
-  prev.addEventListener("click", function () {
+  prev.addEventListener("click",  () => {
     clearTimeout(clear);
     mySlides[currentSlide].style.display = "none";
     dots[currentSlide].classList.remove("switch-to-white");
