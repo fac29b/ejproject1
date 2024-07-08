0cc78346937aeb8729e61b7ee8408990e72dbf94
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-07-08 10:30:27 +0100

    server.js file and submit endpoint 2.0

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
 const PORT = 3001;
+let sql;
+// const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post("/submit", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
 
-app.listen(PORT, ()=> {
-    console.log(`application listening on port ${PORT}`)
-})
+  console.log(`Name: ${name}, Email: ${email}`);
+
+  res.send(`Thank you for your submission, ${name}!`);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`application listening on port ${PORT}`);
+});
